9c547816b64cb6202044b6ea99e3b7eb129b0ab9
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-27 19:11:14 +0200

    Sort component recives array as props from app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Sort from './Sort'
+import {generateArray} from './actions/generateArray'
+import {useState} from 'react'
 
 function App() {
+  const [width, setWidth] = useState(document.body.offsetWidth);
+
+  window.onresize = function () {
+    setWidth(document.body.offsetWidth);
+  };
+  
+  let array = generateArray(Math.floor((width * 0.8) / 12))
 
 
   return (
     <div className="app">
-      <Sort/>
+      <Sort array={array}/>
     </div>
   );
 }
