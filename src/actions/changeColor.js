fbf0d612b5fc653672ea7a4086ed583e62cb7fab
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-27 19:08:22 +0200

    created changeColor action

---
+++ src/actions/changeColor.js
@@ -0,0 +1,13 @@
+export function rechangeColor(x, y) {
+  let bar = document.getElementById(x);
+  bar.style.backgroundColor = '#00A6CB';
+  let bar2 = document.getElementById(y);
+  bar2.style.backgroundColor = '#00A6CB';
+}
+
+export function changeColor(x, y) {
+  let bar = document.getElementById(x);
+  bar.style.backgroundColor = '#D68EC3';
+  let bar2 = document.getElementById(y);
+  bar2.style.backgroundColor = '#D68EC3';
+}
