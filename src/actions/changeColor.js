f2fabefe407d334100cb32ec662883a5c47e0615
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-28 22:45:44 +0200

    fiexd bugs

--- src/actions/changeColor.js
+++ src/actions/changeColor.js
@@ -1,13 +1,25 @@
 export function rechangeColor(x, y) {
-  let bar = document.getElementById(x);
-  bar.style.backgroundColor = '#00A6CB';
-  let bar2 = document.getElementById(y);
-  bar2.style.backgroundColor = '#00A6CB';
+  try {
+    let bar = document.getElementById(x);
+    bar.style.backgroundColor = '#00A6CB';
+    if (y) {
+      let bar2 = document.getElementById(y);
+      bar2.style.backgroundColor = '#00A6CB';
+    }
+  } catch (er) {
+    // console.log(er);
+  }
 }
 
 export function changeColor(x, y) {
-  let bar = document.getElementById(x);
-  bar.style.backgroundColor = '#D68EC3';
-  let bar2 = document.getElementById(y);
-  bar2.style.backgroundColor = '#D68EC3';
+  try {
+    let bar = document.getElementById(x);
+    bar.style.backgroundColor = '#D68EC3';
+    if (y) {
+      let bar2 = document.getElementById(y);
+      bar2.style.backgroundColor = '#D68EC3';
+    }
+  } catch (er) {
+    // console.log(er);
+  }
 }
