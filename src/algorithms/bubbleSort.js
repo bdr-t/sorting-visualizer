c5ee92d744143247bb68266134b94b772e7842f5
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-30 03:27:45 +0200

    refactored BubbleSort

--- src/algorithms/bubbleSort.js
+++ src/algorithms/bubbleSort.js
@@ -1,14 +1,19 @@
-function bubbleSort(array){
+export function bubbleSort(array) {
+  let comparisons = [];
 
-    for(let x = 0; x < array.length-1; x++){
-        for(let y = 0; y < array.length-1-x; y++){
-            if(array[y] > array[y+1] ){
-                let temp = array[y]
-                array[y] = array[y+1]
-                array[y+1] = temp
-            }
-        }
+  for (let x = 0; x < array.length - 1; x++) {
+      let colors = []
+    for (let y = 0; y < array.length - 1 - x; y++) {
+      colors.push([y, y + 1]);
+
+      if (array[y] > array[y + 1]) {
+
+        let temp = array[y];
+        array[y] = array[y + 1];
+        array[y + 1] = temp;
+      }
     }
-
-    return array
+    comparisons.push(colors)
+  }
+  return comparisons;
 }
