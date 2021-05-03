1ec5faa82474e58d1a4d3fc61f05c79d5a2fc716
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-05-03 02:26:57 +0200

    final refactor of insertionSort

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -1,33 +1,31 @@
-export function insertionSort(array) {
-  let animations = [];
-  let comparisons = [];
-  let temps = []
-  let n = array.length;
-  for (let i = 1; i < n; i++) {
-    let anim = [];
-    let colors = [];
-
-    let y = i - 1;
-    while (y > 0) {
-      colors.push([y, y - 1]);
-      y--;
-    }
-    let current = array[i];
-    temps.push(current)
-
-    let j = i - 1;
-
-    while (j > -1 && current < array[j]) {
-      array[j + 1] = array[j];
-      anim.push([...array]);
-      j--;
-    }
-    array[j + 1] = current;
-    anim.push([...array]);
-
-    animations.push(anim);
-
-    comparisons.push(colors);
-  }
-  return [animations, comparisons, temps];
+export function insertionSort(inputArr) {
+  let animations = []
+  let comparisons = []
+  let changes = []
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+          let anim = []
+          let colors = []
+          let swaps = []
+            let current = inputArr[i];
+            let y = i - 1
+            while (y > -1) {
+        colors.push([i, y])
+        y--;
+      }
+            let j = i-1; 
+            while ((j > -1) && (current < inputArr[j])) {
+                swaps.push([i, j])
+                inputArr[j+1] = inputArr[j];
+                anim.push([...inputArr])
+                j--;
+            }
+            inputArr[j+1] = current;
+            anim.push([...inputArr])
+            animations.push(anim)
+            comparisons.push(colors)
+            changes.push(swaps)
+        }
+    return [animations, comparisons, changes ];
 }
+
